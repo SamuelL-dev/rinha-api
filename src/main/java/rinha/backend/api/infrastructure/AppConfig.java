6e41a0c4d9c6bc23c00b2ba76f2b487d556dab4d
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-03-08 23:31:19 -0300

    feat: create config class

---
+++ src/main/java/rinha/backend/api/infrastructure/AppConfig.java
@@ -0,0 +1,14 @@
+package rinha.backend.api.infrastructure;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+}
